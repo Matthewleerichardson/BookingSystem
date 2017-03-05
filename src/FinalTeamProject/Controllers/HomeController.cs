31afb0c1f95522644280e112c248907d0576e1a0
Matthewleerichardson <u1553758@unimail.hud.ac.uk>
2017-03-05 13:53:55 +0000

    Changing the about page to display some customer statistics. showing both the grouped appointments by day and the amount of customers.

--- src/FinalTeamProject/Controllers/HomeController.cs
+++ src/FinalTeamProject/Controllers/HomeController.cs
@@ -3,21 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FinalTeamProject.Data;
+using FinalTeamProject.Models.BookigViewModels;
 
 namespace FinalTeamProject.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly BookingContext _context;
+
+        public HomeController(BookingContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult About()
+        public async Task<ActionResult> About()
         {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
+            IQueryable<CustomerGroup> data =
+                from Appointment in _context.Appointments
+                group Appointment by Appointment.AppointmentDate into dateGroup 
+                select new CustomerGroup()
+                {
+                   AppointmentDate = dateGroup.Key,
+                    CustomerCount = dateGroup.Count()
+                };
+            return View(await data.AsNoTracking().ToListAsync());
         }
 
         public IActionResult Contact()
---
+++ src/FinalTeamProject/Models/BookingViewModels.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FinalTeamProject.Models.BookigViewModels
+{
+    public class CustomerGroup
+    {
+        [DataType(DataType.Date)]
+        public DateTime? AppointmentDate { get; set; }
+
+        public int CustomerCount { get; set; }
+    }
+}
